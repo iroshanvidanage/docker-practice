8e433036d74ab158918a1788f2d40b23be9101da
iroshanav <knightshift.kurosaki@gmail.com>
2024-07-01 00:36:32 +0530

    Updated | compose commands

--- references/S06.sh
+++ references/S06.sh
@@ -1,7 +1,19 @@
 # iroshan vidanage
 
-# docker default command
+# docker default command to start
 docker-compose up
 
 # what i used
 docker-compose -f docker-compose-6-1.yml up -d
+
+# to re build the image
+docker-compose build
+
+# what i used
+docker-compose -f docker-compose-6-1.yml build
+
+# to remove all the compose file data
+docker-compose -f docker-compose-6-1.yml down
+
+#remove the local cache
+docker-compose -f docker-compose-6-2.yml down --rmi local
